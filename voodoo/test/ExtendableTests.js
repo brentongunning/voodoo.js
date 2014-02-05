e71d68ab3ad59edd62e90cbcbd1fa950eb34bbc5
Brenton Gunning <brentongunning@gmail.com>
2014-02-05 00:35:28 -0800

    Add failing test cases for future Extendable.base

--- voodoo/test/ExtendableTests.js
+++ voodoo/test/ExtendableTests.js
@@ -88,3 +88,143 @@ ExtendableTests.prototype.testExtendAnotherExtendable = function() {
   assertTrue('A extended', ab.a);
   assertTrue('B extended', ab.b);
 };
+
+
+/**
+ * Tests that it's always OK to call this.base.X when X is
+ * is defined on an extended type at this level. The function
+ * called should just be a dummy.
+ */
+ExtendableTests.prototype.testDummyBase = function() {
+  done = false;
+
+  Base = voodoo.Extendable.extend({
+    construct: function() {},
+    foo: function() { this.base.foo(); done = true; }
+  });
+
+  new Base().foo();
+  assertTrue('Base.foo() called', done);
+};
+
+
+/**
+ * Tests this.base on an object extended.
+ */
+ExtendableTests.prototype.testBaseWithObject = function() {
+  var fooCallers = [];
+
+  Base = voodoo.Extendable.extend({
+    construct: function() {},
+    foo: function() { fooCallers.push('Base'); }
+  });
+
+  A = Base.extend({
+    foo: function() { this.base.foo(); fooCallers.push('A'); }
+  });
+
+  new A();
+
+  assertEquals(2, fooCallers.length);
+  assertEquals('Base', fooCallers[0]);
+  assertEquals('A', fooCallers[1]);
+};
+
+
+/**
+ * Tests this.base on an Extendable extended.
+ */
+ExtendableTests.prototype.testBaseWithExtendable = function() {
+  var fooCallers = [];
+
+  Base = voodoo.Extendable.extend({
+    construct: function() {},
+    foo: function() { fooCallers.push('Base'); }
+  });
+
+  A = voodoo.Extendable.extend({
+    construct: function() {}
+  });
+  A.prototype.foo = function() {
+    this.base.foo();
+    fooCallers.push('A');
+  };
+
+  Extended = Base.extend(A);
+
+  new Extended();
+
+  assertEquals(2, fooCallers.length);
+  assertEquals('Base', fooCallers[0]);
+  assertEquals('A', fooCallers[1]);
+};
+
+
+/**
+ * Tests this.base on a chain of multiple types.
+ */
+ExtendableTests.prototype.testBaseWithTypeChain = function() {
+  var fooCallers = [];
+
+  Base = voodoo.Extendable.extend({
+    construct: function() {},
+    foo: function() { fooCallers.push('Base'); }
+  });
+
+  A = Base.extend({
+    foo: function() { this.base.foo(); fooCallers.push('A'); }
+  });
+
+  B = A.extend({
+    foo: function() { this.base.foo(); fooCallers.push('B'); }
+  });
+
+  C = B.extend({
+    foo: function() { this.base.foo(); fooCallers.push('C'); }
+  });
+
+  new C();
+
+  assertEquals(4, fooCallers.length);
+  assertEquals('Base', fooCallers[0]);
+  assertEquals('A', fooCallers[1]);
+  assertEquals('B', fooCallers[2]);
+  assertEquals('B', fooCallers[3]);
+};
+
+
+/**
+ * Tests this.base on an object chain created by inserting
+ * one object in the middle of others.
+ */
+ExtendableTests.prototype.testBaseWithInsertedType = function() {
+  var fooCallers = [];
+
+  Base = voodoo.Extendable.extend({
+    construct: function() {}
+  });
+
+  A = Base.extend();
+  A.prototype.foo = function() { this.base.foo(); fooCallers.push('A'); };
+
+  B = Base.extend();
+  B.prototype.foo = function() { this.base.foo(); fooCallers.push('B'); };
+
+  C = Base.extend();
+  C.prototype.foo = function() { this.base.foo(); fooCallers.push('C'); };
+
+  D = Base.extend();
+  D.prototype.foo = function() { this.base.foo(); fooCallers.push('D'); };
+
+  AB = A.extend(B);
+  CD = C.extend(D);
+  ABCD = AB.extend(CD);
+
+  new ABCD();
+
+  assertEquals(4, fooCallers.length);
+  assertEquals('A', fooCallers[0]);
+  assertEquals('B', fooCallers[1]);
+  assertEquals('C', fooCallers[2]);
+  assertEquals('D', fooCallers[3]);
+};
