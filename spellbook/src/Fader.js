dd923111cb09fcace5fc47d3924112e4af96dc88
Brenton Gunning <brentongunning@gmail.com>
2014-02-17 09:46:36 -0800

    Create Hider

--- spellbook/src/Fader.js
+++ spellbook/src/Fader.js
@@ -40,10 +40,40 @@ var FaderView_ = voodoo.View.extend({
 
 
 /**
+ * The stencil view that controls the alpha values of scene meshes.
+ *
+ * @constructor
+ * @private
+ * @extends {voodoo.View}
+ */
+var FaderStencilView_ = voodoo.View.extend({
+
+  load: function() {
+    this.base.load();
+
+    this.scene.on('add', function(e) {
+      e.object.visible = this.model.alpha > 0;
+    });
+  },
+
+  setAlpha: function(alpha) {
+    var sceneObjects = this.scene.objects;
+    for (var i = 0; i < sceneObjects.length; ++i) {
+      sceneObjects[i].visible = this.model.alpha > 0;
+    }
+
+    this.dirty();
+  }
+
+});
+
+
+
+/**
  * Adds functions to fade in and out meshes.
  *
  * Options are:
- *   alpha {number} Initial alpha value.
+ *   alpha {number} Initial alpha value. Default is 0.
  *
  * @constructor
  * @extends {voodoo.Model}
@@ -55,33 +85,44 @@ var Fader = this.Fader = voodoo.Model.extend({
   name: 'Fader',
   organization: 'spellbook',
   viewType: FaderView_,
+  stencilViewType: FaderStencilView_,
 
   initialize: function(options) {
     this.base.initialize(options);
 
-    this.alpha = typeof options.alpha !== 'undefined' ? options.alpha : 0;
-    this.startAlpha = this.alpha;
-    this.targetAlpha = this.alpha;
+    this.alpha_ = typeof options.alpha !== 'undefined' ? options.alpha : 0;
+    this.startAlpha = this.alpha_;
+    this.targetAlpha = this.alpha_;
     this.fadeStartTime = null;
     this.fadeDuration = 0;
+
+    Object.defineProperty(this, 'alpha', {
+      get: function() { return this.alpha_; },
+      set: function(alpha) {
+        this.setAlpha(alpha);
+      },
+      writeable: false
+    });
   },
 
   update: function(deltaTime) {
     this.base.update(deltaTime);
 
-    if (this.alpha !== this.targetAlpha) {
+    if (this.alpha_ !== this.targetAlpha) {
       var now = new Date();
       var duration = now - this.fadeStartTime;
       var t = duration / this.fadeDuration;
 
       if (t < 1.0)
-        this.alpha = this.startAlpha * (1 - t) + this.targetAlpha * t;
-      else this.alpha = this.targetAlpha;
+        this.alpha_ = this.startAlpha * (1 - t) + this.targetAlpha * t;
+      else this.alpha_ = this.targetAlpha;
 
-      if (this.alpha === this.targetAlpha)
+      if (this.alpha_ === this.targetAlpha)
         this.dispatch(new voodoo.Event('fadeEnd', this));
 
-      this.view.setAlpha(this.alpha);
+      this.view.setAlpha(this.alpha_);
+      if (typeof this.stencilView !== 'undefined' && this.stencilView)
+        this.stencilView.setAlpha(this.alpha_);
     }
   }
 
@@ -89,42 +130,42 @@ var Fader = this.Fader = voodoo.Model.extend({
 
 
 /**
-   * Fades in all scene meshes to full alpha.
-   *
-   * @param {number} seconds Animation duration.
-   *
-   * @return {Fader}
-   */
+  * Fades in all scene meshes to full alpha.
+  *
+  * @param {number} seconds Animation duration.
+  *
+  * @return {Fader}
+  */
 Fader.prototype.fadeIn = function(seconds) {
   return this.fadeTo(1.0, seconds);
 };
 
 
 /**
-   * Fades out all scene meshes to zero apha.
-   *
-   * @param {number} seconds Animation duration.
-   *
-   * @return {Fader}
-   */
+  * Fades out all scene meshes to zero apha.
+  *
+  * @param {number} seconds Animation duration.
+  *
+  * @return {Fader}
+  */
 Fader.prototype.fadeOut = function(seconds) {
   return this.fadeTo(0.0, seconds);
 };
 
 
 /**
-   * Fades in all scene meshes to full alpha.
-   *
-   * @param {number} alpha Alpha value from 0-1.
-   * @param {number} seconds Animation duration.
-   *
-   * @return {Fader}
-   */
+  * Fades in all scene meshes to full alpha.
+  *
+  * @param {number} alpha Alpha value from 0-1.
+  * @param {number} seconds Animation duration.
+  *
+  * @return {Fader}
+  */
 Fader.prototype.fadeTo = function(alpha, seconds) {
   if (seconds == 0) {
     this.setAlpha(alpha);
-  } else if (this.alpha != alpha) {
-    this.startAlpha = this.alpha;
+  } else if (this.alpha_ != alpha) {
+    this.startAlpha = this.alpha_;
     this.targetAlpha = alpha;
     this.fadeStartTime = new Date();
     this.fadeDuration = seconds * 1000;
@@ -137,17 +178,27 @@ Fader.prototype.fadeTo = function(alpha, seconds) {
 
 
 /**
-   * Immediately changes the alpha value of all scene meshes.
-   *
-   * @param {number} alpha Alpha value from 0-1.
-   *
-   * @return {Fader}
-   */
+  * Immediately changes the alpha value of all scene meshes.
+  *
+  * @param {number} alpha Alpha value from 0-1.
+  *
+  * @return {Fader}
+  */
 Fader.prototype.setAlpha = function(alpha) {
-  this.alpha = alpha;
+  this.alpha_ = alpha;
   this.targetAlpha = alpha;
 
   this.view.setAlpha(alpha);
+  if (typeof this.stencilView !== 'undefined' && this.stencilView)
+    this.stencilView.setAlpha(alpha);
 
   return this;
 };
+
+
+/**
+ * Get or set the alpha value of all scene meshes.
+ *
+ * @type {number}
+ */
+Fader.prototype.alpha = 0;
---
+++ spellbook/src/Hider.js
@@ -0,0 +1,104 @@
+// ----------------------------------------------------------------------------
+// File: Hider.js
+//
+// Copyright (c) 2014 Voodoojs Authors
+// ----------------------------------------------------------------------------
+
+
+
+/**
+ * The view that controls the visibility of scene meshes.
+ *
+ * @constructor
+ * @private
+ * @extends {voodoo.View}
+ */
+var HiderView_ = voodoo.View.extend({
+
+  load: function() {
+    this.base.load();
+
+    this.scene.on('add', function(e) {
+      e.object.visible = this.model.visible;
+    });
+  },
+
+  setVisible: function(visible) {
+    var sceneObjects = this.scene.objects;
+    for (var i = 0; i < sceneObjects.length; ++i)
+      sceneObjects[i].visible = visible;
+
+    this.dirty();
+  }
+
+});
+
+
+
+/**
+ * Adds functions to show and hide meshes.
+ *
+ * Options are:
+ *   visible {boolean} Initial visibility. Default is true.
+ *
+ * @constructor
+ * @extends {voodoo.Model}
+ *
+ * @param {Object=} opt_options Options object.
+ */
+var Hider = this.Hider = voodoo.Model.extend({
+
+  name: 'Hider',
+  organization: 'spellbook',
+  viewType: HiderView_,
+  stencilViewType: HiderView_,
+
+  initialize: function(options) {
+    this.base.initialize(options);
+
+    this.visible_ = typeof options.visible !== 'undefined' ?
+        options.visible : true;
+
+    Object.defineProperty(this, 'visible', {
+      get: function() { return this.visible_; },
+      set: function(visible) {
+        this.visible_ = visible;
+        this.view.setVisible(visible);
+        if (typeof this.stencilView !== 'undefined' && this.stencilView)
+          this.stencilView.setVisible(visible);
+      },
+      writeable: false
+    });
+  }
+
+});
+
+
+/**
+  * Shows all scene meshes.
+  *
+  * @return {Hider}
+  */
+Hider.prototype.show = function() {
+  this.visible = true;
+  return this;
+};
+
+
+/**
+  * Hides all scene meshes.
+  *
+  * @return {Hider}
+  */
+Hider.prototype.hide = function() {
+  this.visible = false;
+  return this;
+};
+
+
+/**
+ * Get or set the visibility of all scene meshes.
+ *
+ * @type {boolean}
+ */
+Hider.prototype.visible = false;
